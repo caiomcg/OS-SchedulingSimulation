697977df0d5ad43e06d19e8acf81c162678102e0
caiomcg <caiomcg@gmail.com>
2016-09-16 11:35:14 -0300

    Update main and makefile

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
+
+#include "../include/Process.h"
+#include "../include/Utils.h"
 
 int main(int argc, char** argv) {
-    std::cout << "TODO" << std::endl;
+	std::vector<Process> processes;
+
+    if (argc == 1 || argc > 2) {
+    	//add usage
+    	return 1;
+    }
+
+    if (!Utils::extractData(argv[1], processes)) {
+    	//show error
+    	return 1;
+    }
+
+    Utils::print(processes);
+    //call agorithms
+
     return 0;
 }
