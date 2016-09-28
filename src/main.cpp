733e4de9953ffb9b604e26f3de2b9e9853e02c05
caiomcg <caiomcg@gmail.com>
2016-09-28 14:40:44 -0300

    Modify input type

--- src/main.cpp
+++ src/main.cpp
@@ -8,26 +8,24 @@
 #include "../include/FCFS.h"
 #include "../include/SJF.h"
 
-
 int main(int argc, char** argv) {
 	std::vector<Process> processes;
+    unsigned int arrival        = 0;
+    unsigned int span           = 0;
+    unsigned int programCounter = 0;
 
-    if (argc == 1 || argc > 2) {
-    	//add usage
-    	return 1;
-    }
-
-    if (!Utils::extractData(argv[1], processes)) {
-    	//show error
-    	return 1;
+    while(std::cin >> arrival >>span) {
+        processes.push_back(Process(arrival, span, programCounter++));
     }
 
     FCFS* fcfs = new FCFS(processes);
     SJF*  sjf  = new SJF(processes);
+
     fcfs->init();
     sjf->init();
 
     std::cout << fcfs->toString() << std::endl;
+    std::cout << sjf->toString() << std::endl;
 
     delete fcfs;
     delete sjf;
