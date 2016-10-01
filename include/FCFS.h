1cdba8a879dacca53cf9dfe59ed00a6501e0cf4f
caiomcg <caiomcg@gmail.com>
2016-10-01 14:31:12 -0300

    Comment the FCFS implementation

--- include/FCFS.h
+++ include/FCFS.h
@@ -1,3 +1,19 @@
+/**
+ * @class FCFS
+ *
+ * @brief First-Come, First-Served algorithm simulation class
+ *
+ * This Class is responsible for receiving a std::vector of Processes
+ * and run the FCFS algorithm over the input. The user can request the
+ * average response time, the average return time and the average wait
+ * time using getters or the toString method. 
+ *
+ * @note See Process Class implementation to create the std::vector.
+ *
+ * @author Caio Marcelo Campoy Guedes <caiomcg@gmail.com>
+ */
+
+
 #ifndef FCFS_H_
 #define FCFS_H_
 
@@ -10,26 +26,70 @@
 
 class FCFS {
 private:
-	std::vector<Process> _queue;
+	std::vector<Process> _queue; /// The Process queue that will be executed under the algorithm.
 	
-	int _amountOfProcesses;
+	int _amountOfProcesses; /// The amount of process on the process queue.
 
-	double _avgResponse;
-	double _avgReturn;
-	double _avgWait;
+	double _avgResponse; /// Average response time.
+	double _avgReturn; /// Average return time.
+	double _avgWait; /// Average waiting time.
 
+	/**
+	 * @brief Calculate the average time.
+	 * @details Calculate the average time and populate the 
+	 * double variables so the user can access the resulting 
+	 * data.
+	 */
 	void calculateAverageTime();
 	
 public:
+	/**
+	 * @brief Class constructor.
+	 * @details Initialize the class attributes.
+	 * 
+	 * @param processes Process queue to be executed under the algorithm.
+	 */
 	FCFS(std::vector<Process> processes);
+	/**
+	 * @brief Class destructor.
+	 * @details Does base C++ cleanup.
+	 */
 	~FCFS();
 
+	/**
+	 * @brief Called to run the algorithm.
+	 * @details Calls the calculateAverageTime member function wich is responsible
+	 * for executing the algorithm.
+	 */
 	void init();
-	
+
+	/**
+	 * @brief Returns the average response time.
+	 * 
+	 * @return The average response time.
+	 */
 	double getAverageResponse();
+	/**
+	 * @brief Returns the average return time.
+	 * 
+	 * @return The average return time.
+	 */
 	double getAverageReturn();
+	/**
+	 * @brief Returns the average waiting time.
+	 * 
+	 * @return The average waiting time.
+	 */
 	double getAverageWait();
 
+	/**
+	 * @brief Return a formatted string with the algorithm average time.
+	 * @details The string returned will be as follow: FCFS X.X X.X X.X
+	 * where the first result is the average return time, the second is the
+	 * average response time and the third is the average waiting time.
+	 * 
+	 * @return A formatted string.
+	 */
 	std::string toString();
 };
 
--- src/FCFS.cpp
+++ src/FCFS.cpp
@@ -1,44 +1,44 @@
 #include "../include/FCFS.h"
 
 FCFS::FCFS(std::vector<Process> processes) : _queue(processes), _avgResponse(0), _avgReturn(0), _avgWait(0) {
-	_amountOfProcesses = processes.size();
+	_amountOfProcesses = processes.size(); // Initialize amountOfProcesses with the vector size.
 }
 
 FCFS::~FCFS() {}
 
 void FCFS::calculateAverageTime() {
-	unsigned int sum = 0;
-	unsigned int duration;
-	unsigned int clock = _queue[0].getArrivalTime();
+	unsigned int wait     = 0; // Store the sum of the waiting time for all processes.
+	unsigned int duration = 0; // Store the sum of the duration time for all processes.
+	unsigned int clock    = _queue[0].getArrivalTime(); //Stores the current clock time.
 
-	for (int i = 0; i < _queue.size(); i++) {
-		sum  += clock - _queue[i].getArrivalTime();
-		clock += _queue[i].getProcessSpan();
-		duration += _queue[i].getProcessSpan();
+	for (int i = 0; i < _queue.size(); i++) { //loops through the processes queue.
+		wait  += clock - _queue[i].getArrivalTime(); // Add the waiting time of a process.
+		clock += _queue[i].getProcessSpan();         // Update the clock.
+		duration += _queue[i].getProcessSpan();      // Update the total duration
 	}
-	_avgResponse = sum / (double)_queue.size();
-	_avgReturn   = duration / (double)_queue.size() + _avgResponse;
-	_avgWait     = _avgResponse; 
+	_avgResponse = wait / (double)_amountOfProcesses; // Calculate the average response time: ART = (process from 1 to n wait) / amount of processes 
+	_avgReturn   = duration / (double)_amountOfProcesses + _avgResponse; // Calculate the average return time: ART = processes span / amount of process + ART(Average response time)
+	_avgWait     = _avgResponse; // Calculate the average wait time AWT = ART(Average Response time)
 }
 
 void FCFS::init() {
-	this->calculateAverageTime();
+	this->calculateAverageTime(); // Call the member function that runs the algorithm.
 }
 
 double FCFS::getAverageResponse() {
-	return this->_avgResponse;
+	return this->_avgResponse; // Return the Average Response Time
 }
 
 double FCFS::getAverageReturn() {
-	return this->_avgReturn;
+	return this->_avgReturn; // Return the Average Return Time
 }
 
 double FCFS::getAverageWait() {
-	return this->_avgWait;
+	return this->_avgWait; // Return the Average Wait Time
 }
 
 std::string FCFS::toString() {
-	std::stringstream ss;
-	ss << "FCFS " << std::fixed << std::setprecision(1) << _avgReturn << " " << _avgResponse << " " << _avgWait;
-	return ss.str();
+	std::stringstream ss; // Creates a string stream.
+	ss << "FCFS " << std::fixed << std::setprecision(1) << _avgReturn << " " << _avgResponse << " " << _avgWait; // Format the string.
+	return ss.str(); // Return the strin.
 }
