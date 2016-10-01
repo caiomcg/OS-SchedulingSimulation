58b1785c8bc4f7e8955292a9a19c6f5d9c3c41dd
caiomcg <caiomcg@gmail.com>
2016-10-01 14:43:26 -0300

    Comment the Utils namespace implementation

--- include/Utils.h
+++ include/Utils.h
@@ -1,3 +1,14 @@
+/**
+ * @brief The utils namespace implements useful functions for debugging
+ *
+ * The utils namespace is composed of functions used for extracting
+ * data from files or printing the processes allocated in a std::vector. 
+ *
+ * @note Utils function may not be used on the algorithms implementation.
+ *
+ * @author Caio Marcelo Campoy Guedes <caiomcg@gmail.com>
+ */
+
 #ifndef UTILS_H_
 #define UTILS_H_
 
@@ -7,8 +18,25 @@
 #include "Process.h"
 
 namespace Utils {
+	/**
+	 * @brief Extract the data from a file and store in a std::vector of processes.
+	 * @details Open a text file and fetch the data from it creating process objects and
+	 * pushing them to the std::vector.
+	 * 
+	 * @param filePath The path where the file is located
+	 * @param output The std::vetor where the objects will be pushed to.
+	 * 
+	 * @return True if no problem occured.
+	 */
 	bool extractData(const char* filePath, std::vector<Process>& output);
+	/**
+	 * @brief Print the processes from a std::vector.
+	 * @details The output will be formatted as follows:
+	 * Process - [Program counter] - arrival: arrival time span: process span. 
+	 * 
+	 * @param p The std::vector to be printed.
+	 */
 	void print(std::vector<Process>& p);
 }
 
-#endif
+#endif // define Utils
--- src/Utils.cpp
+++ src/Utils.cpp
@@ -1,25 +1,27 @@
 #include "../include/Utils.h"
 
 bool Utils::extractData(const char* filePath, std::vector<Process>& output){
-	std::ifstream input(filePath, std::ifstream::in);
+	std::ifstream input(filePath, std::ifstream::in); // Try to open the input stream from the passed file path.
 
-	if (!input.is_open()) {
+	if (!input.is_open()) { // Check if the file was successfully opened.
 		return false;
 	}
 
-	unsigned int arrival = 0;
-	unsigned int span = 0;
-	unsigned int programCounter = 0;
+	unsigned int arrival = 0; 		 // Store the arrival time of a process.
+	unsigned int span = 0; 			 // Store the process time span.
+	unsigned int programCounter = 0; // Store the process counter.
 
-	while (input >> arrival >> span) {
-		output.push_back(Process(arrival, span, programCounter));
-		programCounter++;
+	while (input >> arrival >> span) { // Read the data from the file.
+		output.push_back(Process(arrival, span, programCounter)); // Create a Process object with the data and push it to the vector.
+		programCounter++; // Increase the program counter.
 	}
+
+	input.close(); // Close the file stream.
 	return true;
 }
 
 void Utils::print(std::vector<Process>& p) {
-	for (int i = 0; i < p.size(); i++) {
-		std::cout << "Process - [" << p[i].getProgramCounter() << "] - arrival: " << p[i].getArrivalTime() << " span: " << p[i].getProcessSpan() << std::endl;
+	for (int i = 0; i < p.size(); i++) { // Loops through the process queue.
+		std::cout << "Process - [" << p[i].getProgramCounter() << "] - arrival: " << p[i].getArrivalTime() << " span: " << p[i].getProcessSpan() << std::endl; // Print the process to the standard output.
 	}
 }
