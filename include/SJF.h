6974c22e281193711066d8a4f7b9e6cfa3674441
caiomcg <caiomcg@gmail.com>
2016-10-01 15:32:56 -0300

    Comment the SJF implementation and update unused variables and variable names

--- include/SJF.h
+++ include/SJF.h
@@ -1,3 +1,19 @@
+/**
+ * @class SJF
+ *
+ * @brief Short Job First algorithm simulation class
+ *
+ * This Class is responsible for receiving a std::vector of Processes
+ * and run the SJF algorithm over the input. The user can request the
+ * average response time, the average return time and the average wait
+ * time using getters or the toString method. 
+ *
+ * @note See Process Class implementation to create the std::vector.
+ *
+ * @author Caio Marcelo Campoy Guedes <caiomcg@gmail.com>
+ */
+
+
 #ifndef SJF_H_
 #define SJF_H_
 
@@ -11,35 +27,118 @@
 #include "Process.h"
 #include "Utils.h"
 
+typedef enum {
+	sort,
+	initialSort
+} Predicate; /// Predicate selection enumeration.
+
 class SJF{
 private:
-	std::vector<Process> _queue;
-	std::vector<Process> _waiting;
+	std::vector<Process> _queue; /// The Process queue that will be executed under the algorithm.
+	std::vector<Process> _waiting; /// The Sorted Process queue that will be executed under the algorithm.
 
-	int _amountOfProcesses;
+	double _avgResponse; /// Average response time.
+	double _avgReturn; /// Average return time.
+	double _avgWait; /// Average waiting time.
 
-	double _avgResponse;
-	double _avgReturn;
-	double _avgWait;
-
+	/**
+	 * @brief The sort predicate for std::sort
+	 * @details Order the process by process time span.
+	 * 
+	 * @param first The first process.
+	 * @param second The second process.
+	 * 
+	 * @return True if sort is needed.
+	 */
 	static bool sortPredicate(const Process& first, const Process& second);
+	/**
+	 * @brief The sort predicate for std::sort
+	 * @details Order the process by process time span or arrival time.
+	 * 
+	 * @param first The first process.
+	 * @param second The second process.
+	 * 
+	 * @return True if sort is needed.
+	 */
 	static bool initialSortPredicate(const Process& first, const Process& second);
-	
- 	void sortVector(std::vector<Process>& vector, unsigned int offset);
 
+	/**
+	 * @brief Sort a std::vector of process.
+	 * @details Sort the std::vector of process depending on the sorting offset
+	 * and the sort predicate to be used.
+	 * 
+	 * @param vector The std::vector that will be sorted
+	 * @param int The offset from the beginning of the vector.
+	 * @param predicate The sort predicate to use. See Predicate enum for further
+	 * information.
+	 */
+ 	void sortVector(std::vector<Process>& vector, unsigned int offset, Predicate predicate);
+
+ 	/**
+ 	 * @brief Remove an amount of elements from the std::vector.
+ 	 * @details Removes the elements based on the distance parameter, which means
+ 	 * that it will remove from the first to first + distance elements.
+ 	 * 
+ 	 * @param vector The std::vector that the element will be taken from.
+ 	 * @param int The distance from the beginning to remove.
+ 	 */
  	void removeFromVector(std::vector<Process>& vector, unsigned int distance);
 
+ 	/**
+	 * @brief Calculate the average time.
+	 * @details Calculate the average time and populate the 
+	 * double variables so the user can access the resulting 
+	 * data.
+	 */
 	void calculateAverageTime();
 public:
+	/**
+	 * @brief Class constructor.
+	 * @details Initialize the class attributes.
+	 * 
+	 * @param processes Process queue to be executed under the algorithm.
+	 */
 	SJF(std::vector<Process> processes);
+	/**
+	 * @brief Class destructor.
+	 * @details Does base C++ cleanup.
+	 */
 	~SJF();
 
+	/**
+	 * @brief Called to run the algorithm.
+	 * @details Calls the calculateAverageTime member function wich is responsible
+	 * for executing the algorithm.
+	 */
 	void init();
-	
+
+	/**
+	 * @brief Returns the average response time.
+	 * 
+	 * @return The average response time.
+	 */
 	double getAverageResponse();
+	/**
+	 * @brief Returns the average return time.
+	 * 
+	 * @return The average return time.
+	 */
 	double getAverageReturn();
+	/**
+	 * @brief Returns the average waiting time.
+	 * 
+	 * @return The average waiting time.
+	 */
 	double getAverageWait();
 
+	/**
+	 * @brief Return a formatted string with the algorithm average time.
+	 * @details The string returned will be as follow: SJF X.X X.X X.X
+	 * where the first result is the average return time, the second is the
+	 * average response time and the third is the average waiting time.
+	 * 
+	 * @return A formatted string.
+	 */
 	std::string toString();
 };
 
--- src/SJF.cpp
+++ src/SJF.cpp
@@ -1,92 +1,93 @@
 #include "../include/SJF.h"
 
 
-SJF::SJF(std::vector<Process> processes) : _queue(processes), _avgResponse(0), _avgReturn(0), _avgWait(0) {
-	_amountOfProcesses = processes.size();
-}
+SJF::SJF(std::vector<Process> processes) : _queue(processes), _avgResponse(0), _avgReturn(0), _avgWait(0) {}
 
 SJF::~SJF() {}
 
 bool SJF::sortPredicate(const Process& first, const Process& second){  
-	return first.getProcessSpan() < second.getProcessSpan();
+	return first.getProcessSpan() < second.getProcessSpan(); // Swap elements if the first process time span is smaller than the second.
 }
 
 bool SJF::initialSortPredicate(const Process& first, const Process& second) {
- 	if (first.getArrivalTime() == second.getArrivalTime()) {
- 		return first.getProcessSpan() < second.getProcessSpan();
+ 	if (first.getArrivalTime() == second.getArrivalTime()) { // Check if processes have the same arrival time.
+ 		return first.getProcessSpan() < second.getProcessSpan();  // Swap elements if the first process time span is smaller than the second.
  	}
-
-	return first.getArrivalTime() < second.getArrivalTime();
+	return first.getArrivalTime() < second.getArrivalTime(); // Swap elements if the first arrival time is smaller than the second.
 }
  
-void SJF::sortVector(std::vector<Process>& vector, unsigned int offset) {
-	std::sort(vector.begin() + offset, vector.end(), SJF::sortPredicate);
+void SJF::sortVector(std::vector<Process>& vector, unsigned int offset, Predicate predicate) {
+	if (predicate == sort) { // Check the predicate to be used.
+		std::sort(vector.begin() + offset, vector.end(), SJF::sortPredicate); // Sort vectors with predicate: sortPredicate.
+	} else {
+		std::sort(vector.begin() + offset, vector.end(), SJF::initialSortPredicate); // Sort vectors with predicate: initialSortPredicate.
+	}
 }
 
 void SJF::removeFromVector(std::vector<Process>& vector, unsigned int distance) {
-	vector.erase(vector.begin(), vector.begin() + distance);
+	vector.erase(vector.begin(), vector.begin() + distance); // Erase a determined amount of elements.
 }
 
 void SJF::calculateAverageTime(){
-	unsigned int sum      = 0;
-	unsigned int running  = 0;
-	unsigned int pushed   = 0;
-	unsigned int duration = 0;
-	unsigned int clock    = _queue[0].getArrivalTime();
+	unsigned int wait     = 0; // Store the sum of the waiting time for all processes.
+	unsigned int running  = 0; // Store the current running process.
+	unsigned int pushed   = 0; // Store the amount of elements that were pushed to the new queue.
+	unsigned int duration = 0; // Store the sum of the duration time for all processes.
+	unsigned int clock    = _queue[0].getArrivalTime(); //Stores the current clock time.
 
-	while (!_queue.empty()) {
-		pushed = 0;
+	while (!_queue.empty()) { // Runs until the queue is empty
+		pushed = 0; // Resets the amount of pushed processes.
 
-		for (int i = 0; i < _queue.size(); i++) {
-			if (_queue[i].getArrivalTime() == clock) {
-				_waiting.push_back(_queue[i]);
-				pushed++;
+		for (int i = 0; i < _queue.size(); i++) { // Loops through the initial queue.
+			if (_queue[i].getArrivalTime() == clock) { // Check if the current process have the same arrival time as the clock.
+				_waiting.push_back(_queue[i]); //Push the process to the new queue.
+				pushed++; // Increments the amount of pushed elements.
 			} else {
-				break;
+				break; // Stop pushing element.
 			}	
 		}
 
-		if (clock > _waiting[running].getProcessSpan()) {
-			running++;
+		if (clock > _waiting[running].getProcessSpan()) { // Check if the processe has stopped its execution.
+			running++; // Move the execution to a new process.
 		}
 
-		this->removeFromVector(_queue, pushed);
-		this->sortVector(_waiting, running + 1);
+		this->removeFromVector(_queue, pushed); // Remove the elements that were pushed from the main queue.
+		this->sortVector(_waiting, running + 1, sort); // Sort the new queue based on the current running process.
 
-		clock++;
+		clock++; // Increments the clock time.
 	}
 
-	clock = _waiting[0].getArrivalTime(); //Restarts the clock
+	clock = _waiting[0].getArrivalTime(); // Restarts the clock.
 
-	for (int i = 0; i < _waiting.size(); i++) {
-		sum  += clock - _waiting[i].getArrivalTime();
-		clock += _waiting[i].getProcessSpan();
-		duration += _waiting[i].getProcessSpan();
+	for (int i = 0; i < _waiting.size(); i++) { // Loop through the new sorted queue.
+		wait  += clock - _waiting[i].getArrivalTime(); // Add the waiting time of a process.
+		clock += _waiting[i].getProcessSpan();         // Update the clock.
+		duration += _waiting[i].getProcessSpan();      // Update the total duration
 	}
-	_avgResponse = sum / (double)_waiting.size();
-	_avgReturn   = duration / (double)_waiting.size() + _avgResponse;
-	_avgWait     = _avgResponse;
+	_avgResponse = wait / (double)_waiting.size(); // Calculate the average response time: ART = (process from 1 to n wait) / amount of processes 
+	_avgReturn   = duration / (double)_waiting.size() + _avgResponse; // Calculate the average return time: ART = processes span / amount of process + ART(Average response time)
+	_avgWait     = _avgResponse; // Calculate the average wait time AWT = ART(Average Response time)
 }
 
 void SJF::init() {
-	std::sort(_queue.begin(), _queue.end(), SJF::initialSortPredicate); //CHANGE TO FUNCTION
-	this->calculateAverageTime();
+	this->sortVector(_queue, 0, initialSort); // Pre sort the vector.
+	this->calculateAverageTime(); // Call the member function that runs the algorithm.
 }
 	
 double SJF::getAverageResponse() {
-	return this->_avgResponse;
+	return this->_avgResponse; // Return the Average Response Time
 }
 
 double SJF::getAverageReturn() {
-	return this->_avgReturn;
+	return this->_avgReturn; // Return the Average Return Time
 }
 
 double SJF::getAverageWait() {
-	return this->_avgWait;
+	return this->_avgWait; // Return the Average Wait Time
 }
 
 std::string SJF::toString() {
-	std::stringstream ss;
-	ss << "SJF " << std::fixed << std::setprecision(1) << _avgReturn << " " << _avgResponse << " " << _avgWait;
-	return ss.str();
+	std::stringstream ss; // Creates a string stream.
+	ss << "SJF " << std::fixed << std::setprecision(1) << _avgReturn << " " << _avgResponse << " " << _avgWait; // Format the string.
+	return ss.str(); // Return the strin.
 }
